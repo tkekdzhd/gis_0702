d2c5fe3e506624b07bd876eef3d2d9f2fa98a5bf
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-09 11:48:57 +0900

    0709 commit 2

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,3 +3,10 @@ from django.db import models #models는 패키지명
 # Create your models here.
 class NewModel(models.Model):
     text = models.CharField(max_length=255, null=False)
+
+
+# 모델의 변경을 감지
+# makemigrations
+
+# 실제로 만들어 주는것
+# migrate
